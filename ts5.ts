95431c6fe40f2b9b15b8b9f7d7428678d0b5255f
sagun <sagun_90@hotmail.com>
2018-01-29 10:26:47 -0600

    robust functions

--- ts5.ts
+++ ts5.ts
@@ -1,5 +1,13 @@
-function myfunction(param1: number, paran2: number): void {
+function myfunction(param1: number, param2: number): void {
     return param1+param2;
 }
 
 myfunction (123,123);
+
+function myfunc(...args:any[]){
+    for (let arg of args){
+        console.log(typeof(arg));
+    }
+}
+
+myfunc(1,2,3,'a','b','c',true);
